c93d9ad94032598437047e1d11bb1470529a2d6d
makinggoodsoftware <alberto.gutierrez.campo@gmail.com>
2015-11-04 13:34:23 +0000

    WIP adding tests to the deals developer

--- src/test/integration/com/mgs/rbsnov/logic/DealsDeveloperSpecification.groovy
+++ src/test/integration/com/mgs/rbsnov/logic/DealsDeveloperSpecification.groovy
@@ -37,7 +37,7 @@ class DealsDeveloperSpecification extends Specification {
         ] as Set
     }
 
-    def "should develop all playable cards from the last player" (){
+    def "should develop the last playable cards from the last player" (){
         when:
         Set<FinishedDeal> finishedDeals = dealsDeveloper.develop(
                 dealInProgressFactory.threeCardsDeal(Player.NORTH, Card.TEN_OF_CLUBS, Card.TWO_OF_CLUBS, Card.FIVE_OF_HEARTS),
@@ -53,5 +53,46 @@ class DealsDeveloperSpecification extends Specification {
                 )
         ] as Set
     }
+
+
+    def "should develop the only playable cards from the last player" (){
+        when:
+        Set<FinishedDeal> finishedDeals = dealsDeveloper.develop(
+                dealInProgressFactory.threeCardsDeal(Player.NORTH, Card.TEN_OF_CLUBS, Card.TWO_OF_CLUBS, Card.FIVE_OF_HEARTS),
+                new Hands ([] as Set, [Card.EIGHT_OF_CLUBS, Card.ACE_OF_DIAMONDS] as Set, [] as Set, [] as Set)
+        )
+
+        then:
+        finishedDeals == [
+                new FinishedDeal(
+                        new Deal(Card.TEN_OF_CLUBS, Card.TWO_OF_CLUBS, Card.FIVE_OF_HEARTS, Card.EIGHT_OF_CLUBS),
+                        new PlayersScore(0, 0, 1, 0),
+                        Player.NORTH
+                )
+        ] as Set
+    }
+
+    def "should develop the only playable cards from the last two player" (){
+        when:
+        Set<FinishedDeal> finishedDeals = dealsDeveloper.develop(
+                dealInProgressFactory.twoCardsDeal(Player.NORTH, Card.TEN_OF_CLUBS, Card.TWO_OF_CLUBS),
+                new Hands ([Card.THREE_OF_CLUBS, Card.ACE_OF_CLUBS] as Set, [Card.EIGHT_OF_CLUBS, Card.ACE_OF_DIAMONDS] as Set, [] as Set, [] as Set)
+        )
+
+        then:
+        finishedDeals == [
+                new FinishedDeal(
+                        new Deal(Card.TEN_OF_CLUBS, Card.TWO_OF_CLUBS, Card.THREE_OF_CLUBS, Card.EIGHT_OF_CLUBS),
+                        new PlayersScore(0, 0, 0, 0),
+                        Player.NORTH
+                ),
+                new FinishedDeal(
+                        new Deal(Card.TEN_OF_CLUBS, Card.TWO_OF_CLUBS, Card.ACE_OF_CLUBS, Card.EIGHT_OF_CLUBS),
+                        new PlayersScore(0, 0, 0, 0),
+                        Player.SOUTH
+                )
+        ] as Set
+    }
+
 
 }
