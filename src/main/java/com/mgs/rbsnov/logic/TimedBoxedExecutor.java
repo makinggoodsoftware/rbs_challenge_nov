7cb10b70a956cd11522dad1d996c28c8f9bfc0de
makinggoodsoftware <alberto.gutierrez.campo@gmail.com>
2015-11-18 17:23:44 +0000

    WIP Improving the TimedBoxedExecutor

--- src/main/java/com/mgs/rbsnov/logic/TimedBoxedExecutor.java
+++ src/main/java/com/mgs/rbsnov/logic/TimedBoxedExecutor.java
@@ -3,16 +3,17 @@ package com.mgs.rbsnov.logic;
 import org.apache.log4j.Logger;
 
 import java.util.concurrent.*;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class TimedBoxedExecutor <T, R> {
     private static final Logger LOG = Logger.getLogger(TimedBoxedExecutor.class);
-    private final Function<T, R> toExecute;
+    private final Consumer<T> toExecute;
     private final Function<T, R> onTimeout;
     private final int timeoutMs;
     private final ExecutorService executor;
 
-    public TimedBoxedExecutor(Function<T, R> toExecute, Function<T, R> onTimeout, int timeoutMs, ExecutorService executor) {
+    public TimedBoxedExecutor(Consumer<T> toExecute, Function<T, R> onTimeout, int timeoutMs, ExecutorService executor) {
         this.toExecute = toExecute;
         this.onTimeout = onTimeout;
         this.timeoutMs = timeoutMs;
@@ -21,14 +22,21 @@ public class TimedBoxedExecutor <T, R> {
 
     public R execute (T input){
         try {
-            Future<R> future = executor.submit(() -> toExecute.apply(input));
+            Future<R> future = executor.submit(dummyFunction(toExecute, input));
             future.get(timeoutMs, TimeUnit.MILLISECONDS);
-            return toExecute.apply(input);
+            throw new IllegalStateException("Should run forever!");
         } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
         } catch (TimeoutException e) {
             LOG.warn("timeout!");
             return onTimeout.apply(input);
         }
+    }
+
+    private Callable<R> dummyFunction(Consumer<T> toExecute, T input) {
+        return () -> {
+            toExecute.accept(input);
+            throw new IllegalStateException("Should run forever!");
+        };
     }
 }
