e516e994e3cce0189df53223e187484f43a020b7
makinggoodsoftware <alberto.gutierrez.campo@gmail.com>
2015-11-19 22:33:22 +0000

    Improving tests

--- src/test/integration/com/mgs/rbsnov/logic/CardsDealerSpecification.groovy
+++ src/test/integration/com/mgs/rbsnov/logic/CardsDealerSpecification.groovy
@@ -15,25 +15,24 @@ class CardsDealerSpecification extends Specification {
     @Resource
     CardsSetBuilder cardsSetBuilder
 
-    def "should follow restrictions" (){
+    def "should remember who has initially discarded cards" (){
         when:
         Map<Player, Set<Card>> cards = cardsDealer.deal(
                 Player.all(Player.SOUTH),
-                cardsSetBuilder.allCards().findAll{it != Card.TWO_OF_CLUBS},
+                cardsSetBuilder.allCards().findAll{it != Card.TWO_OF_CLUBS && it != Card.ACE_OF_CLUBS},
                 [
                         (Player.EAST): [Card.ACE_OF_SPADES, Card.ACE_OF_DIAMONDS, Card.ACE_OF_CLUBS] as Set
                 ]
         )
 
         then:
-        cards[Player.NORTH].size() == 13
+        cards[Player.NORTH].size() == 12
         cards[Player.EAST].size() == 12
         cards[Player.SOUTH].size() == 13
         cards[Player.WEST].size() == 13
 
         cards[Player.EAST].contains(Card.ACE_OF_SPADES)
         cards[Player.EAST].contains(Card.ACE_OF_DIAMONDS)
-        cards[Player.EAST].contains(Card.ACE_OF_CLUBS)
-
+        ! cards[Player.EAST].contains(Card.ACE_OF_CLUBS)
     }
 }
